82e1691f8dc3b397cee0c6d808eafc4ab43d4790
gaspergigio <alejandro.arribillaga@gmail.com>
2022-06-22 18:28:51 -0300

    Tasks updaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent{
 
   //TODO: blog-003 (High): Implementar Angular Universal
   //TODO: blog-013 (Low): Agregar Google analytics
-  //TODO: blog-002 (Urgent): respetar fecha - testing general.
   //TODO: blog-004: (High): Agregar dominio
+  //TODO: blog-005: (High): Use an slug en dynamic routes
 }
 
